411fa73a37fb2a1eae7e1daa4cf0aabccf86bd94
Edward <edward.vercoe@gmail.com>
2019-07-10 00:50:06 +1000

    added tilt and drag

--- js/discogs.js
+++ js/discogs.js
@@ -33,10 +33,19 @@ const addResults = function (results) {
   results.forEach(result => {
     resultsTag.innerHTML = resultsTag.innerHTML +
     `
-	<div class="record">
+	<div class="record draggable tilt" data-tilt>
 	 	<img src="${result.coverimg}">
 	</div>
 	`
+  VanillaTilt.init(document.querySelectorAll(".tilt"), {
+		max: 35,
+		speed: 1500,
+    glare: true,
+    "max-glare": 0.8,
+    reset: false,
+    scale: 1.2,
+    transition: false
+	});
   })
 
 }
@@ -46,7 +55,49 @@ const displayRecords = function() {
   discogsData()
     .then(results => {
     	addResults(results)
-  		})  
+
+      document.addEventListener("mouseover", function(event) {
+        if (event.target && event.target.classList.contains("welcome")) {
+      // selects all images and puts in an array
+          const images = slideArea.querySelectorAll(".record")
+
+      // when mouseover put images in a random place
+
+          images.forEach(image => {
+            const x = Math.floor(Math.random() * Math.floor(80))
+            const y = Math.floor(Math.random() * Math.floor(60))
+
+            image.style.left = `${x}%`
+            image.style.top = `${y}%`
+
+
+
+          })
+          event.target.classList.remove("welcome")
+        }
+      })
+
+          let zCount = 1
+      document.addEventListener("mousedown", function(event) {
+        if (event.target && event.target.classList.contains("record")) {
+
+          zCount = zCount + 1
+
+          const record = event.target
+          record.style.zIndex = zCount
+
+        }
+      })
+      document.addEventListener("onmouseup", function(event) {
+        if (event.target && event.target.classList.contains("record")) {
+          const record = event.target
+          record.style.zIndex = 0
+        }
+      })
+
+
+  		})
 }
+
 
 displayRecords()
